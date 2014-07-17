54f73802083bd81929f6bf38328536b8ec91c761
Katherine Crowson <crowsonkb@gmail.com>
2014-07-16 20:02:35 -0700

    Tweak HTML/CSS

--- assets/chat.js
+++ assets/chat.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 	var ws = new WebSocket("ws://"+window.location.host+"/chat");
 	ws.onmessage = function(event) {
-		$("#message").text(event.data)
+		$("#messages").append($("<p>").text(event.data))
 	};
 
 	$("#input").keypress(function(event) {
 		if (event.which == 13) {
 			ws.send($("#input").val())
+			$("#input").val("")
 		}
 	});
 });
