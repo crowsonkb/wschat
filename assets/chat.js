62107ecf4eaf8c8a008a1bc0b69c7c1cc37a8242
Katherine Crowson <crowsonkb@gmail.com>
2014-07-17 09:36:01 -0700

    Use wss: if UI is loaded over https:

--- assets/chat.js
+++ assets/chat.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-	var ws = new WebSocket("ws://"+window.location.host+"/chat");
+	var protocol = "ws:";
+	if (window.location.protocol == "https:") {
+		protocol = "wss:"
+	}
+	var ws = new WebSocket(protocol+"//"+window.location.host+"/chat");
 	ws.onmessage = function(event) {
 		$("#messages").append($("<p>").text(event.data))
 	};
